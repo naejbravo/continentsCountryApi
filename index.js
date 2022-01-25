ba13f2c4a7c702b7f422dd8950b90461f4d9d6a3
jean bravo <naejbravo@gmail.com>
2022-01-25 23:11:14 +0100

    fix url vercelAngular

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:4200"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:4200",
+      "https://angular-continent-countries.vercel.app/",
+    ],
     credentials: true,
   })
 );
