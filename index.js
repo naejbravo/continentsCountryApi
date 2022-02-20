ac4043e48b9e1f5a859c9750c76d4863c20794d3
jean bravo <naejbravo@gmail.com>
2022-02-20 01:29:30 +0100

    last now

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: [
-      "https://continents-country-api.vercel.app"
+      "angular-continent-countries.vercel.app"
       // "https://angular-continent-countries.vercel.app", "http://localhost:4200"
     ],
     credentials: true,
